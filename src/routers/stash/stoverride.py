e3f959047ba20726b4609be1cde63b9c80b94264
wangqs <wqs1639@gmail.com>
2024-03-21 09:41:11 +0800

    feat: add a stash overwrite API that preserves inline comments

--- src/routers/stash/stoverride.py
+++ src/routers/stash/stoverride.py
@@ -11,14 +11,17 @@ logger = logging.getLogger(__file__)
 
 
 @router.get("/override")
-def Override(
+def override(
     url: HttpUrl = Query(..., description="覆写文件订阅地址"),
     name: str | None = Query(None),
     desc: str | None = Query(None),
     category: str | None = Query(None, description="分类名称"),
     icon: str | None = Query(None),
 ):
-    """覆盖覆写文件"""
+    """覆盖覆写文件
+    \f
+    使用 yaml 进行解析， 无法保留注释
+    """
     logger.debug(f"url: {url}")
     res = httpx.get(str(url), follow_redirects=True)
     res.raise_for_status()
@@ -33,4 +36,52 @@ def Override(
     if icon is not None:
         dom["icon"] = icon
     content = yaml.safe_dump(dom, allow_unicode=True)
+    return PlainTextResponse(content=content)
+
+
+@router.get("/override/v2")
+def override_v2(
+    url: HttpUrl = Query(..., description="覆写文件订阅地址"),
+    name: str | None = Query(None),
+    desc: str | None = Query(None),
+    category: str | None = Query(None, description="分类名称"),
+    icon: str | None = Query(None),
+):
+    """保留注释行， 仅保留整行的注释， 不支持保留同行文本内容后的注释
+    \f
+    按行分析文件， 保留未匹配的文本
+    不按照 yaml 解析
+    """
+
+    logger.debug(f"url: {url}")
+    res = httpx.get(str(url), follow_redirects=True)
+    res.raise_for_status()
+    lines = []
+    for line in res.text.split("\n"):
+        if line.strip().startswith("#"):
+            lines.append(line)
+            continue
+        text = line.strip()
+        if text.startswith("name") and name is not None:
+            line = f"name:{name}"
+            name = None
+        if text.startswith("desc") and desc is not None:
+            line = f"desc:{desc}"
+            desc = None
+        if text.startswith("category") and category is not None:
+            line = f"category:{category}"
+            category = None
+        if text.startswith("icon") and icon is not None:
+            line = f"icon:{icon}"
+            icon = None
+        lines.append(line)
+    if name is not None:
+        lines.insert(0, f"name:{name}")
+    if desc is not None:
+        lines.insert(0, f"desc:{desc}")
+    if category is not None:
+        lines.insert(0, f"category:{category}")
+    if icon is not None:
+        lines.insert(0, f"icon:{icon}")
+    content = "\n".join(lines)
     return PlainTextResponse(content=content)
