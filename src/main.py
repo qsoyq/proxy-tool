88f7d9eea7c7af3172c5fe15c4f2cf8e792c3132
wangqs <wqs1639@gmail.com>
2024-04-06 06:21:47 +0800

    feat: add an api convert xml to json

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ import routers.stash.stoverride
 import routers.network.dns.doh
 import routers.network.url.redirect
 import routers.bilibili.live.room
+import routers.convert.xml
 
 from settings import AppSettings
 
@@ -23,6 +24,7 @@ app.include_router(routers.stash.stoverride.router, prefix=api_prefix)
 app.include_router(routers.network.dns.doh.router, prefix=api_prefix)
 app.include_router(routers.network.url.redirect.router, prefix=api_prefix)
 app.include_router(routers.bilibili.live.room.router, prefix=api_prefix)
+app.include_router(routers.convert.xml.router, prefix=api_prefix)
 
 
 @cmd.command()
---
+++ src/routers/convert/xml.py
@@ -0,0 +1,30 @@
+import xmltodict
+import json
+from pydantic import BaseModel, Field
+import logging
+from fastapi import APIRouter, Body, Query
+
+router = APIRouter(tags=["convert.xml"], prefix="/convert/xml")
+
+logger = logging.getLogger(__file__)
+
+
+class XMLConvertRes(BaseModel):
+    content: str = Field(..., description="json字符串")
+
+
+class XMLConvertReq(BaseModel):
+    content: str = Field(..., description="xml字符串")
+
+
+@router.get("/json", response_model=XMLConvertRes)
+def to_json(content: str = Query(..., description="xml字符串")):
+    """将传入的 xml 字符串转成 json字符串并返回"""
+    return {"content": json.dumps(xmltodict.parse(content), ensure_ascii=False)}
+
+
+@router.post("/json", response_model=XMLConvertRes)
+def to_json_v2(req: XMLConvertReq = Body(...)):
+    """将传入的 xml 字符串转成 json字符串并返回"""
+    content = req.content
+    return {"content": json.dumps(xmltodict.parse(content), ensure_ascii=False)}
