c269ae712f4ca702f4e37b73e7e82886e049cc41
wangqs <wqs1639@gmail.com>
2024-04-15 19:07:44 +0800

    feat: supplement methods to the reverse proxy api

--- src/routers/network/proxy/reverse.py
+++ src/routers/network/proxy/reverse.py
@@ -9,6 +9,13 @@ logger = logging.getLogger(__file__)
 
 
 @router.get("/reverse/{host}/{path:path}", summary="转发请求")
+@router.post("/reverse/{host}/{path:path}", summary="转发请求")
+@router.put("/reverse/{host}/{path:path}", summary="转发请求")
+@router.delete("/reverse/{host}/{path:path}", summary="转发请求")
+@router.options("/reverse/{host}/{path:path}", summary="转发请求")
+@router.head("/reverse/{host}/{path:path}", summary="转发请求")
+@router.patch("/reverse/{host}/{path:path}", summary="转发请求")
+@router.trace("/reverse/{host}/{path:path}", summary="转发请求")
 async def forwarding(
     req: Request,
     host: str = Path(..., description="请求主机地址"),
@@ -17,6 +24,7 @@ async def forwarding(
     """转发请求"""
     method = req.method
     scheme = req.url.scheme
+    scheme = "https"
     query = req.url.query
     headers = {}
     headers = dict(req.headers)
