f142a4ea3e62ae3c5a436d4796c0df51ff97237c
wangqs <wqs1639@gmail.com>
2024-05-13 08:14:43 +0800

    feat: add notification.push.v2 API to support concurrent push messages

--- src/routers/notifications/push.py
+++ src/routers/notifications/push.py
@@ -9,6 +9,11 @@ from http import HTTPStatus
 
 from fastapi import APIRouter, Query
 from fastapi.responses import JSONResponse, RedirectResponse
+from concurrent.futures import ThreadPoolExecutor
+from schemas import ErrorDetail
+
+
+executor = ThreadPoolExecutor(thread_name_prefix="notifications.push.")
 
 
 class GmailOauth2File(BaseModel):
@@ -127,7 +132,7 @@ router = APIRouter(tags=["notifications.push"], prefix="/notifications")
 logger = logging.getLogger(__file__)
 
 
-@router.post("/push")
+@router.post("/push", summary="消息推送")
 def push(messages: PushMessages):
     """推送消息到各平台
     Gmail推送消息对账号密码有要求, 需要开启二步验证后使用应用程序专用密码或使用 OAuth2 令牌.
@@ -150,6 +155,43 @@ def push(messages: PushMessages):
             logger.warning(e, exc_info=True)
             loc.append(current)
             details.append({"type": "value_error", "loc": loc, "message": f"{e}"})
+    if details:
+        return JSONResponse(content={"detail": details}, status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+    return {}
+
+
+@router.post("/push/v2", summary="消息推送v2")
+def push_v2(messages: PushMessages):
+    """推送消息到各平台
+    Gmail推送消息对账号密码有要求, 需要开启二步验证后使用应用程序专用密码或使用 OAuth2 令牌.
+    采用并发处理多条消息
+    """
+
+    def handle_message(index: int, message: PushMessage) -> ErrorDetail | None:
+        detail = None
+        loc = ["body", "messages", index]
+        try:
+            current = ""
+            if message.telegram:
+                current = "telegram"
+                message.telegram.push()
+            if message.gmail:
+                current = "gmail"
+                message.gmail.push()
+            if message.bark:
+                current = "bark"
+                message.bark.push()
+        except Exception as e:
+            logger.warning(e, exc_info=True)
+            loc.append(current)
+            detail = ErrorDetail(**{"type": "value_error", "loc": loc, "message": f"{e}"})
+        return detail
+
+    global executor
+    with executor:
+        arguments = [(i, x) for i, x in enumerate(messages)]
+        results = executor.map(handle_message, *arguments)
+    details = [x for x in results if x]
     if details:
         return JSONResponse(content={"detail": details}, status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
     return {}
