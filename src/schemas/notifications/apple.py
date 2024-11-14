793583e4ae51fa303dcb1be797ed68f478bda0ea
wangqs <wqs1639@gmail.com>
2024-11-14 22:33:25 +0800

    feat: add thread-id field to notifications.push API apple messages.

--- src/schemas/notifications/apple.py
+++ src/schemas/notifications/apple.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Dict, Any
 from functools import lru_cache
 import httpx
 from pydantic import BaseModel, Field, HttpUrl
@@ -88,6 +89,7 @@ class ApplePushMessage(ApplePushExtParams, ApplePushAuthParams):
     部分默认参数取自 bark
     """
 
+    thread_id: str | None = Field(None, alias="thread-id")  # type: ignore
     device_token: str = Field(...)
     aps: AppleAPNSMessage
 
@@ -109,7 +111,9 @@ class ApplePushMessage(ApplePushExtParams, ApplePushAuthParams):
         }
 
         ext = ApplePushExtParams(**self.dict()).dict(exclude_none=True)
-        payload = {"aps": self.aps.dict(exclude_none=True, by_alias=True)}
+        payload: Dict[str, Any] = {"aps": dict(self.aps.dict(exclude_none=True, by_alias=True))}
+        if self.thread_id:
+            payload["thread-id"] = self.thread_id
         payload.update(ext)
         resp = httpx.Client(http2=True).post(url, json=payload, headers=headers)
         resp.raise_for_status()
