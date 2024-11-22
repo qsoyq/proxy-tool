32e69fed8485352236a16d67287893b59ddbdfa2
wangqs <wqs1639@gmail.com>
2024-11-23 00:56:37 +0800

    feat: support passing pagination parameters for the v2ex.my.topics API and return an indicator of whether there is a next page.

--- src/routers/v2ex/my.py
+++ src/routers/v2ex/my.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import logging
 import httpx
 from pydantic import BaseModel, Field
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Query
+from dataclasses import dataclass
 
 
 class Topic(BaseModel):
@@ -15,6 +16,13 @@ class Topic(BaseModel):
 
 class GetTopicsRes(BaseModel):
     topics: list[Topic]
+    has_next_page: bool = Field(..., description="是否还有下一页")
+
+
+@dataclass
+class GetTopicsData:
+    topics: list[Topic]
+    has_next_page: bool
 
 
 router = APIRouter(tags=["v2ex"], prefix="/v2ex/my")
@@ -22,13 +30,16 @@ router = APIRouter(tags=["v2ex"], prefix="/v2ex/my")
 logger = logging.getLogger(__file__)
 
 
-def get_topics(session_key: str) -> list[Topic]:
+def get_topics(session_key: str, page: int = 1) -> GetTopicsData:
     topics = []
     url = "https://www.v2ex.com/my/topics"
-    res = httpx.get(url, cookies={"A2": session_key})
+    res = httpx.get(url, params={"p": page}, cookies={"A2": session_key})
     res.raise_for_status()
-    soup = BeautifulSoup(res.text)
+    soup = BeautifulSoup(res.text, features="lxml")
     items = soup.find_all("div", class_="cell item")
+    ele = soup.select_one('td[title="Next Page"]')
+    has_next_page = True if ele and "disable_now" not in ele.attrs.get("class", []) else False
+
     for item in items:
         link = item.find("a", class_="topic-link")
         title = link.text
@@ -36,9 +47,13 @@ def get_topics(session_key: str) -> list[Topic]:
         lastTouchedStr = item.find("span", class_="topic_info").find("span").attrs["title"]
         last_touched = int(parser.parse(lastTouchedStr).timestamp())
         topics.append(Topic(id=tid, title=title, lastTouchedStr=lastTouchedStr, last_touched=last_touched))
-    return topics
+    return GetTopicsData(topics=topics, has_next_page=has_next_page)
 
 
 @router.get("/topics", summary="收藏主题列表", response_model=GetTopicsRes)
-def my_topics(session_key: str = Header(..., alias="A2", description="session, 对应 v2ex 网页请求中的 Cookie.A2字段")):
-    return {"topics": get_topics(session_key)}
+def my_topics(
+    session_key: str = Header(..., alias="A2", description="session, 对应 v2ex 网页请求中的 Cookie.A2字段"),
+    page: int = Query(1, alias="p", description="页码"),
+):
+    res = get_topics(session_key, page)
+    return {"topics": res.topics, "has_next_page": res.has_next_page}
