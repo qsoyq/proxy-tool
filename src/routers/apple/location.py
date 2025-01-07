99e9954e51b893f14230368140d243f29e7d1822
wangqs <wqs1639@gmail.com>
2025-01-08 04:57:47 +0800

    fix typo

--- src/routers/apple/location.py
+++ src/routers/apple/location.py
@@ -8,6 +8,6 @@ router = APIRouter(tags=["tool"], prefix="/apple")
 logger = logging.getLogger(__file__)
 
 
-@router.get("/location/{code}", summary="返回路径种的地区代码")
+@router.get("/location/{code}", summary="返回路径中的地区代码")
 def code(code: str = Path(..., description="地区代码， 见 https://www.geonames.org/countries/")):
     return PlainTextResponse(code)
