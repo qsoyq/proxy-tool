51e6ace30f8942b38801b6f4ceb84ecb4a803aa5
qsoyq <github@19940731.xyz>
2025-04-15 02:00:12 +0800

    feat: add apple.ics.calander api

--- src/main.py
+++ src/main.py
@@ -32,6 +32,7 @@ import routers.nga.thread
 import routers.v2ex.nodes
 import routers.v2ex.my
 import routers.apple.location
+import routers.apple.ics
 import routers.iptv.sub
 from settings import AppSettings
 
@@ -60,6 +61,7 @@ app.include_router(routers.clash.basic.router, prefix=api_prefix)
 app.include_router(routers.clash.config.router, prefix=api_prefix)
 app.include_router(routers.stash.stoverride.router, prefix=api_prefix)
 app.include_router(routers.apple.location.router, prefix=api_prefix)
+app.include_router(routers.apple.ics.router, prefix=api_prefix)
 app.include_router(routers.iptv.sub.router, prefix=api_prefix)
 
 run_at_ts = int(time.time())
---
+++ src/routers/apple/ics.py
@@ -0,0 +1,26 @@
+import logging
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+from datetime import datetime, timedelta
+from ics import Calendar, Event
+from ics.alarm.display import DisplayAlarm
+
+
+router = APIRouter(tags=["Utils"], prefix="/apple")
+
+logger = logging.getLogger(__file__)
+
+
+@router.get("/ics/calander", summary="Apple日历订阅示例")
+def calander():
+    tomorrow = datetime.now() + timedelta(days=1)
+    c = Calendar()
+    e = Event()
+    e.name = "My cool event"
+    e.description = "A meaningful description"
+    e.begin = tomorrow.replace(hour=8, minute=0, second=0, microsecond=0)
+    e.end = tomorrow.replace(hour=20, minute=0, second=0, microsecond=0)
+    e.alarms = [DisplayAlarm(timedelta(minutes=-10))]
+    c.events.add(e)
+
+    return PlainTextResponse(c.serialize())
