615002c66951d7cb76a04d35334e1020a59cce64
qsoyq <github@19940731.xyz>
2025-04-23 23:02:07 +0800

    fix: issued_o may be None

--- src/schemas/network/ssl.py
+++ src/schemas/network/ssl.py
@@ -5,20 +5,20 @@ class SSLCertSchema(BaseModel):
     host: str
     resolved_ip: str
     issued_to: str
-    issued_o: str
-    issuer_c: object | None = Field(None)
-    issuer_o: str
-    issuer_ou: object | None = Field(None)
-    issuer_cn: str
-    cert_sn: str
-    cert_sha1: str
-    cert_alg: str
+    issued_o: str | None = Field(None)
+    issuer_c: str | None = Field(None)
+    issuer_o: str | None = Field(None)
+    issuer_ou: str | None = Field(None)
+    issuer_cn: str | None = Field(None)
+    cert_sn: str | None = Field(None)
+    cert_sha1: str | None = Field(None)
+    cert_alg: str | None = Field(None)
     cert_ver: int
-    cert_sans: str
+    cert_sans: str | None = Field(None)
     cert_exp: bool
     cert_valid: bool
-    valid_from: str
-    valid_till: str
+    valid_from: str | None = Field(None)
+    valid_till: str | None = Field(None)
     validity_days: int
     days_left: int
     valid_days_to_expire: int
