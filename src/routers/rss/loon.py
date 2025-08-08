2ed67a5c28e83778677f66becdfe6afdf766ee75
qsoyq <github@19940731.xyz>
2025-08-09 04:23:23 +0800

    feat: add JSONFeed response structure endpoint for Loon plugin RSS subscription

--- src/routers/rss/loon.py
+++ src/routers/rss/loon.py
@@ -1,15 +1,17 @@
-import datetime
 import logging
 import asyncio
-import dateparser
-import pytz
-import httpx
-from fastapi import APIRouter, Request, Response, Query
-import feedgen.feed
+from datetime import datetime
 
+import pytz
+import feedgen.feed
+import dateparser
+import httpx
+
+from fastapi import APIRouter, Request, Response, Query
+from schemas.rss.jsonfeed import JSONFeed
+from responses import PrettyJSONResponse
 
 router = APIRouter(tags=["RSS"], prefix="/rss/loon")
-
 logger = logging.getLogger(__file__)
 
 
@@ -32,7 +34,7 @@ async def get_ipx_info(url: str, useragent: str) -> dict:
     return meta
 
 
-@router.get("/ipx", summary="Loon插件RSS订阅")
+@router.get("/ipx/v1", summary="Loon插件RSS订阅", include_in_schema=False)
 async def ipx(
     req: Request,
     url_list: list[str] = Query(...),
@@ -65,7 +67,7 @@ async def ipx(
         if dt:
             updated = timezone.localize(dt)
         else:
-            updated = datetime.datetime.now()
+            updated = datetime.now()
 
         entry = fg.add_entry()
         entry.id(f"{name}-{homepage}-{date}")
@@ -75,3 +77,49 @@ async def ipx(
         entry.link(href=href)
     rss_xml = fg.rss_str(pretty=True)
     return Response(content=rss_xml, media_type="application/xml")
+
+
+@router.get("/ipx", summary="Loon插件RSS订阅", response_model=JSONFeed, response_class=PrettyJSONResponse)
+async def ipx_jsonfeed(
+    req: Request,
+    url_list: list[str] = Query(...),
+    ua: str = Query("StashCore/2.7.1 Stash/2.7.1 Clash/1.11.0", description="访问插件地址时使用的 user-agent"),
+):
+    """订阅 loon 插件更新
+
+    - 根据 date 字段提取发布时间
+    """
+    host = req.url.hostname
+    ret = await asyncio.gather(*[get_ipx_info(url, useragent=ua) for url in url_list])
+    items: list = []
+    feed = {
+        "version": "https://jsonfeed.org/version/1",
+        "title": "Loon 插件更新订阅",
+        "description": "",
+        "home_page_url": "https://docs.19940731.xyz",
+        "feed_url": f"{req.url.scheme}://{host}/api/rss/loon/ipx?{req.url.query}",
+        "icon": "https://nsloon.app/img/favicon.png",
+        "favicon": "https://nsloon.app/img/favicon.png",
+        "items": items,
+    }
+
+    timezone = pytz.timezone("Asia/Shanghai")
+    for item in ret:
+        name, homepage, date, href = item.get("name"), item.get("homepage", ""), item.get("date"), item["href"]
+        icon, author, desc = item.get("icon", ""), item.get("author", ""), item.get("desc", "")
+        if not name or not date:
+            continue
+        if isinstance(date, str):
+            date = dateparser.parse(date)
+            if date:
+                date = timezone.localize(date).strftime("%Y-%m-%d %H:%M:%S%Z")
+        payload = {
+            "id": f"{name}-{homepage}-{date}",
+            "title": f"{name}",
+            "url": href,
+            "date_published": date,
+            "content_text": desc,
+            "author": {"avatar": icon, "url": homepage, "name": author},
+        }
+        items.append(payload)
+    return feed
