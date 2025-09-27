e6e10f681c27283bb3262b056ba74e656a54e836
qsoyq <github@19940731.xyz>
2025-09-27 09:23:26 +0800

    feat: add url path regex match condition for rss filter middleware

--- src/middlewares/rss.py
+++ src/middlewares/rss.py
@@ -18,7 +18,9 @@ logger = logging.getLogger(__file__)
 
 
 def add_middleware(app: FastAPI):
-    app.add_middleware(FeedFilterMiddleware)
+    app.add_middleware(TelegramFeedFilterMiddleware)
+    app.add_middleware(NGAFeedFilterMiddleware)
+    app.add_middleware(NodeseekFeedFilterMiddleware)
     app.add_middleware(AddTwitterHTMLFeedMiddleware)
     app.add_middleware(UpdateTelegraphHTMLFeedMiddleware)
 
@@ -110,45 +112,41 @@ class UpdateTelegraphHTMLFeedMiddleware(BaseHTTPMiddleware):
         return response
 
 
-class FeedFilterMiddleware(BaseHTTPMiddleware):
-    BLOCK_TAG = ["#广告", "#互推", "#频道互推", "#群组互推"]
-    BLOCK_CONTENT = (
-        "TG必备的搜索引擎，极搜帮你精准找到，想要的群组、频道、音乐 、视频",
-        "https://hongxingdl.com",
-        "搜 蒸蒸日上 概率有5元猫卡",
-        "<code>ikelee</code>",
-    )
+class BaseFeedFilterMiddleware(BaseHTTPMiddleware):
+    BLOCK_TAG: list[str] = []
+    BLOCK_CONTENT: list[str] = []
 
-    BLOCK_REGEX_CONTENT = (r"(?i)HostDZire", "预制菜")
+    BLOCK_REGEX_CONTENT: str = ""
+    BLOCK_REGEX_TITLE: str = ""
 
-    BLOCK_REGEX_TITLE = (r"(?i)HostDZire", "预制菜")
+    MATCH_URL_PATTERN = r""
 
     def filter_by_block(self, item: dict):
         tags = item["tags"] or []
-        for tag in FeedFilterMiddleware.BLOCK_TAG:
+        for tag in self.__class__.BLOCK_TAG:
             if tag in tags:
-                logger.debug(f"[FeedFilterMiddleware] skip by block tag matched: {tag}")
+                logger.debug(f"[{self.__class__.__name__}] skip by block tag matched: {tag}")
                 return False
 
-        for block in FeedFilterMiddleware.BLOCK_CONTENT:
+        for block in self.__class__.BLOCK_CONTENT:
             if item["content_html"] and block in item["content_html"]:
-                logger.debug(f"[FeedFilterMiddleware] skip by block content matched: {block}")
+                logger.debug(f"[{self.__class__.__name__}] skip by block content matched: {block}")
                 return False
             if item["content_text"] and block in item["content_text"]:
-                logger.debug(f"[FeedFilterMiddleware] skip by block content matched: {block}")
+                logger.debug(f"[{self.__class__.__name__}] skip by block content matched: {block}")
                 return False
 
-        for pattern in FeedFilterMiddleware.BLOCK_REGEX_CONTENT:
+        for pattern in self.__class__.BLOCK_REGEX_CONTENT:
             if item["content_html"] and re.search(pattern, item["content_html"]):
-                logger.debug(f"[FeedFilterMiddleware] skip by regex content matched: {pattern}")
+                logger.debug(f"[{self.__class__.__name__}] skip by regex content matched: {pattern}")
                 return False
             if item["content_text"] and re.search(pattern, item["content_text"]):
-                logger.debug(f"[FeedFilterMiddleware] skip by regex content matched: {pattern}")
+                logger.debug(f"[{self.__class__.__name__}] skip by regex content matched: {pattern}")
                 return False
 
-        for pattern in FeedFilterMiddleware.BLOCK_REGEX_TITLE:
+        for pattern in self.__class__.BLOCK_REGEX_TITLE:
             if item["title"] and re.search(pattern, item["title"]):
-                logger.debug(f"[FeedFilterMiddleware] skip by regex title matched: {pattern}")
+                logger.debug(f"[{self.__class__.__name__}] skip by regex title matched: {pattern}")
                 return False
         return True
 
@@ -157,17 +155,43 @@ class FeedFilterMiddleware(BaseHTTPMiddleware):
     ) -> Response | PrettyJSONResponse:
         response = await call_next(request)
         path = request.url.path
+        result = re.match(self.__class__.MATCH_URL_PATTERN, path)
         ct = response.headers.get("content-type")
-        if ct and ct.startswith("application/json") and path.startswith("/api/rss/"):
+        if ct and ct.startswith("application/json") and result:
             response_body = b""
             async for chunk in response.body_iterator:  # type: ignore
                 response_body += chunk
             body = json.loads(response_body)
             headers = dict(response.headers)
             headers.pop("content-length", None)
-
             if body.get("version") == "https://jsonfeed.org/version/1":
                 body["items"] = list(filter(lambda x: self.filter_by_block(x), body["items"]))
 
             return PrettyJSONResponse(body, status_code=response.status_code, headers=headers)
         return response
+
+
+class TelegramFeedFilterMiddleware(BaseFeedFilterMiddleware):
+    BLOCK_TAG = ["#广告", "#互推", "#频道互推", "#群组互推"]
+    BLOCK_CONTENT = [
+        "TG必备的搜索引擎，极搜帮你精准找到，想要的群组、频道、音乐 、视频",
+        "https://hongxingdl.com",
+        "搜 蒸蒸日上 概率有5元猫卡",
+        "<code>ikelee</code>",
+    ]
+
+    MATCH_URL_PATTERN = r"/api/rss/telegram/"
+
+
+class NodeseekFeedFilterMiddleware(BaseFeedFilterMiddleware):
+    BLOCK_REGEX_CONTENT = r"(?i)HostDZire"
+    BLOCK_REGEX_TITLE = r"(?i)HostDZire"
+
+    MATCH_URL_PATTERN = r"/api/rss/nodeseek/"
+
+
+class NGAFeedFilterMiddleware(BaseFeedFilterMiddleware):
+    BLOCK_REGEX_CONTENT = "预制菜"
+    BLOCK_REGEX_TITLE = "预制菜"
+
+    MATCH_URL_PATTERN = r"/api/rss/nga/"
