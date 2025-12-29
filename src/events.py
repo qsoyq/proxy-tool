b17f7849fa830e9a903f7b8eb0297d8ad90ee98f
qsoyq <github@19940731.xyz>
2025-12-29 17:19:34 +0800

    refactor: improve lifespan management by using AsyncExitStack and streamline background_gc function

--- src/events.py
+++ src/events.py
@@ -1,39 +1,39 @@
 import gc
 import logging
 import asyncio
-from contextlib import asynccontextmanager
-import rssapi.core.events
+from contextlib import asynccontextmanager, AsyncExitStack
+from rssapi.core.events import lifespan as rssapi_lifespan
 from fastapi import FastAPI
 from schemas.ping import get_default_memory
 from settings import AppSettings
-from utils.nga import NgaToolkit  # type:ignore
 
 logger = logging.getLogger(__file__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await startup_event(app)
-    yield
-    await shutdown(app)
+    async with AsyncExitStack() as stack:
+        await stack.enter_async_context(rssapi_lifespan(app))
+        await startup_event(app)
+        yield
+        await shutdown(app)
+
+
+async def background_gc():
+    while True:
+        memory = get_default_memory()
+        memory_percent = float(memory.percent[:-1])
+        settings = AppSettings()
+        if memory_percent >= settings.gc_trigger_memory_percent_limit:
+            logger.debug(
+                f"[background_gc]: trigger gc collect because memory exceeds 80, current: {memory_percent}",
+            )
+            gc.collect()
+        await asyncio.sleep(settings.gc_trigger_memory_percent_interval)
 
 
 async def startup_event(app: FastAPI):
-    async def background_gc():
-        while True:
-            memory = get_default_memory()
-            memory_percent = float(memory.percent[:-1])
-            settings = AppSettings()
-            if memory_percent >= settings.gc_trigger_memory_percent_limit:
-                logger.debug(
-                    f"[background_gc]: trigger gc collect because memory exceeds 80, current: {memory_percent}",
-                )
-                gc.collect()
-            await asyncio.sleep(settings.gc_trigger_memory_percent_interval)
-
     app.state.background_gc_task = asyncio.create_task(background_gc(), name="background_gc")
-    asyncio.create_task(asyncio.to_thread(NgaToolkit.get_smiles), name="get_smiles")
-    await rssapi.core.events.startup_event(app)
 
 
 async def shutdown(app: FastAPI):
